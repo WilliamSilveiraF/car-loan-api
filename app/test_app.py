a624ab611c214a1715be3935cbc1a9c8feca755e
williamsilveiraf <williamsilveiraf@hotmail.com>
2022-02-28 00:44:28 -0300

    feat(api): add test_payment2

--- app/test_app.py
+++ app/test_app.py
@@ -6,3 +6,8 @@ class TestPayment(unittest.TestCase):
         monthlyPayment =  payment.payment(0.08, 10, -10000, 0, 1)
         monthlyPayment = round(monthlyPayment, 2)
         self.assertEqual(monthlyPayment, 1030.16, "\n\napp.src.payment - Error: monthly payment must be equal 1030.16")
+
+    def test_payment2(self):
+        monthlyPayment = payment.payment(0.06, 216, 0, -50000, 0)
+        monthlyPayment = round(monthlyPayment, 2)
+        self.assertEqual(monthlyPayment, 129.08,"\n\napp.src.payment - Error: monthly payment must be equal 129.08")
