e58039d5047b8d8cea50c34948eb83c99146647c
williamsilveiraf <williamsilveiraf@hotmail.com>
2022-02-28 00:33:12 -0300

    feat(api): add test struct

---
+++ app/test_app.py
@@ -0,0 +1,8 @@
+import unittest
+from src import payment
+
+class TestPayment(unittest.TestCase):
+    def test_payment(self):
+        monthlyPayment =  payment.payment(0.08, 10, -10000, 0, 1)
+        monthlyPayment = round(monthlyPayment, 2)
+        self.assertEqual(monthlyPayment, 1030.16, "\n\napp.src.payment - Error: monthly payment must be equal 1030.16")
